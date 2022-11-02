ff4110de3f64691f7d50e73ba19e5a472c4181e1
eloiv <eloividal2001@gmail.com>
2022-11-02 18:43:38 +0100

    HTTPRequest hecho 100%

--- HybridServer/src/es/uvigo/esei/dai/hybridserver/Launcher.java
+++ HybridServer/src/es/uvigo/esei/dai/hybridserver/Launcher.java
@@ -23,15 +23,6 @@ public class Launcher {
 	public static void main(String[] args) {
 		
 
-		String requestText = "GET /hello/world.html?country=Spain&province=Ourense&city=Ourense HTTP/1.1\r\n" +
-				"Host: localhost\r\n" +
-				"Accept: text/html\r\n" +
-				"Accept-Encoding: gzip,deflate\r\n";
-
-
-		HTTPRequest a = new HTTPRequest(requestText);
-		
-		a.getResourcePath();
 		
 	}
 }
