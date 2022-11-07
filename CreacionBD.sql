91b03f85f5b1751a1469ef21ed49910abcfec2be
megahelio <47276914+megahelio@users.noreply.github.com>
2022-11-07 13:07:19 +0100

    Update CreacionBD.sql

--- CreacionBD.sql
+++ CreacionBD.sql
@@ -1,49 +1,26 @@
--- phpMyAdmin SQL Dump
--- version 5.2.0
--- https://www.phpmyadmin.net/
---
--- Servidor: 127.0.0.1
--- Tiempo de generación: 02-11-2022 a las 20:25:31
--- Versión del servidor: 10.4.24-MariaDB
--- Versión de PHP: 8.1.6
-
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
-
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8mb4 */;
 
---
--- Base de datos: `daipages`
---
-CREATE DATABASE IF NOT EXISTS `daipages` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
-USE `daipages`;
+CREATE DATABASE IF NOT EXISTS `hstestdb` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+USE `hstestdb`;
 
--- --------------------------------------------------------
+CREATE USER 'hsdb'@'%' IDENTIFIED VIA mysql_native_password USING '***';GRANT USAGE ON *.* TO 'hsdb'@'%' REQUIRE NONE WITH MAX_QUERIES_PER_HOUR 0 MAX_CONNECTIONS_PER_HOUR 0 MAX_UPDATES_PER_HOUR 0 MAX_USER_CONNECTIONS 0;GRANT ALL PRIVILEGES ON `hstestdb`.* TO 'hsdb'@'%';
+REVOKE ALL PRIVILEGES ON  `hstestdb`.* FROM 'hsdb'@'%';  GRANT ALL PRIVILEGES ON  `hstestdb`.* TO 'hsdb'@'%'; 
 
---
--- Estructura de tabla para la tabla `pages`
---
-
-DROP TABLE IF EXISTS `pages`;
-CREATE TABLE `pages` (
-  `uuid` varchar(500) NOT NULL,
+CREATE TABLE `html` (
+  `uuid` char(36) NOT NULL,
   `content` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
---
--- Índices para tablas volcadas
---
 
---
--- Indices de la tabla `pages`
---
-ALTER TABLE `pages`
-  ADD PRIMARY KEY (`uuid`) USING HASH;
+ALTER TABLE `html`
+  ADD PRIMARY KEY (`uuid`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
