3514c413fd37bbf9a040d288271c7c70f82f362e
megahelio <47276914+megahelio@users.noreply.github.com>
2022-11-08 15:01:30 +0100

    Revert "Update ServiceThread.java"
    This reverts commit d69d6a290fda49e7087e6adfdddc88ff3cb3c6f8.

--- HybridServer/src/es/uvigo/esei/dai/hybridserver/ServiceThread.java
+++ HybridServer/src/es/uvigo/esei/dai/hybridserver/ServiceThread.java
@@ -1,62 +1,15 @@
 package es.uvigo.esei.dai.hybridserver;
 
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.io.Reader;
-import java.io.Writer;
 import java.net.Socket;
 
-import es.uvigo.esei.dai.hybridserver.http.HTTPParseException;
-import es.uvigo.esei.dai.hybridserver.http.HTTPRequest;
-import es.uvigo.esei.dai.hybridserver.http.HTTPResponse;
-import es.uvigo.esei.dai.hybridserver.http.HTTPResponseStatus;
-
 public class ServiceThread implements Runnable {
-    private Socket socket;
-    private Dao dao;
-    private HTTPRequest request;
-    private HTTPResponse response;
 
-    /**
-     * @param socket
-     * @param dao
-     */
     public ServiceThread(Socket socket, Dao dao) {
-        this.socket = socket;
-        this.dao = dao;
-        response = new HTTPResponse();
-
-        try (Reader inputReader = new InputStreamReader(socket.getInputStream())) {
-            try {
-                request = new HTTPRequest(inputReader);
-            } catch (HTTPParseException e) {
-                response.setStatus(HTTPResponseStatus.S400);
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
     }
 
     @Override
     public void run() {
-
-        switch (request.getMethod()) {
-            case POST:
-
-                break;
-            case GET:
-
-                break;
-            case DELETE:
-
-                break;
-            default:
-
-                break;
-        }
-
+        
     }
 
 }
